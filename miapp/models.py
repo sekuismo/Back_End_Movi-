2866e93ec98d335131fc2205eac330dd4f22caa8
Alumnos Santiago Centro <alumnos09@inacap.cl>
2023-06-13 20:10:22 -0400

    avance base

--- miapp/models.py
+++ miapp/models.py
@@ -2,30 +2,38 @@ from django.db import models
 
 
 
-class User(models.Model):
+class Usuarios(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    password = models.CharField(max_length=40,default='')
-    email= models.CharField(max_length=50)
+    password = models.CharField(max_length=60)
+    email = models.CharField(max_length=50,unique=True)#campo único
     avatar = models.CharField(max_length=100)
+    country = models.CharField(max_length=2)
+    date_added = models.DateTimeField()
     def __str__(self):
         return self.name
 
-#es importante que el  parámetro related_name se llame de la misma forma que la instancia de la serialización de 
-# la llave foránea 
 
-class Movie(models.Model):
-    title=models.CharField(max_length=30)
-    description = models.CharField(max_length = 1000)
-    url= models.CharField(max_length=100,default='' )
-    image = models.CharField(max_length=60)
+class ListaDePeliculas(models.Model):
+    id = models.AutoField(primary_key=True)
+    is_viewed = models.BooleanField()
+    is_erased = models.BooleanField()
+    date_added = models.DateTimeField()
+    user = models.ForeignKey(Usuarios,on_delete=models.CASCADE)
+    movie = models.ForeignKey('Peliculas',on_delete=models.CASCADE)
     def __str__(self):
-        return self.title
+        return f'{self.user} : {self.movie}'
+
+class Peliculas(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=800)
+    year = models.IntegerField()
+    language = models.CharField(max_length=2)
+    genre_name = models.CharField(max_length=100)
+    genre_id = models.IntegerField()
+    img_url = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
     
 
     #El ISVIEWED debe estar en la entidad débil MovieList.
-class MovieList(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='movie_lists')
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie_lists')
-    isViewed = models.BooleanField(default=False)
-    def __str__(self):
-        return f" {self.movie.title} {self.isViewed}"
